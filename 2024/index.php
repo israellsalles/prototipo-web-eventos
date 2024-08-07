b039ed1269a932388a4aa37ab29fded50a34b771
Israel Sales <israelsales@outlook.com>
2024-08-07 17:31:05 -0300

    Feito

---
+++ 2024/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php include('./snippets/head.html'); ?>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="70">
+
+    <!-- Barra de Navegação -->
+    <?php include('./snippets/nav.html'); ?>
+
+    <!-- Sessão Home -->
+    <?php include('./snippets/section-home.html'); ?>
+
+    <!-- Sessão Sobre -->
+    <?php include('./snippets/section-sobre.html'); ?>
+
+    <!-- Sessão de Programação -->
+    <?php include('./snippets/section-programacao.html'); ?>
+
+    <!-- Seção Palestrantes -->
+    <?php include('./snippets/section-palestrantes.html'); ?>
+
+    <!-- Sessão dos Patrocinadores -->
+    <?php include('./snippets/section-patrocinadores.html'); ?>
+
+    <!-- Modal de Galeria -->
+    <?php include('./snippets/modal-galeria.html'); ?>
+
+    <!-- Modal de Download -->
+    <?php include('./snippets/modal-download.html'); ?>
+
+    <!-- Modal de Versões Anteriores -->
+    <?php include('./snippets/modal-versoes.html'); ?>
+
+    <!-- Botão de Inscrição no rodapé para smartphone. -->
+    <a href="https://paginaexterna.com" class="floating-button" id="floatingButton" target="_blank">
+        <i class="fas fa-pencil-alt"></i>
+    </a>
+
+    <!-- Footer -->
+    <?php include('./snippets/footer.html'); ?>
+
+    <!-- Chamada dos Scripts -->
+    <?php include('./snippets/scripts.html'); ?>
+</body>
+
+</html>
+</body>
